c7aca8f947eb67f3ed8315156a8a8e3b91a8dcda
Vidur Maheshawari <vidurm09@gmail.com>
2015-06-25 16:21:42 -0700

    Commented Drivecode

--- sampleCode/drivebase_code.c
+++ sampleCode/drivebase_code.c
@@ -1,17 +1,31 @@
+/*
+	TODO:
+	Add Holonomic code
+*/
+
 void tank(){
-	motor[RB] = vexRT[Ch2];
-	motor[RF] = vexRT[Ch2];
-	motor[LF] = motor[LB] = vexRT[Ch3];
+	motor[RB] = motor[RF] = vexRT[Ch2]; //Right side of the drive
+	motor[LF] = motor[LB] = vexRT[Ch3]; //Left side of the drive
 }
 
 void arcade() {
-	motor[RB] = motor[RF] = (vexRT[Ch3] - vexRT[Ch1]);
-	motor[LB] = motor[LF] = (vexRT[Ch3] + vexRT[Ch1]);
+	motor[RB] = motor[RF] = (vexRT[Ch3] - vexRT[Ch1]); //Right side of the drive
+	motor[LB] = motor[LF] = (vexRT[Ch3] + vexRT[Ch1]); //Left side of the drive
 }
 
 void hDrive() {
 	tank();
-	motor[s] = (vexRT[Btn6U] - vexRT[Btn5U])*127;
+	motor[s] = (vexRT[Btn6U] - vexRT[Btn5U])*127; //Strafe Wheel controlled by bumpers
+	/*
+		Equivalent code:
+		if(vexRT[Btn6U]){
+			motor[s] = 127;
+		} else if(vexRT[Btn5U]) {
+			motor[s] = 127;
+		} else {
+			motor[s] = 0;
+		}
+	*/
 }
 
 void mecanum() {
@@ -21,7 +35,7 @@ void mecanum() {
 	motor[LB] =  vexRT[Ch3] + vexRT[Ch1] - vexRT[Ch4];
 }
 
-void drive(bool isTank) {
+void drive(bool isTank) { //one drive function that can toggle between drives
 		if(isTank) {
 			tank();
 		} else {
