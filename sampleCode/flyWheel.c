7a31b89ad3f78a0bf21bd1499856454743ba85c0
vidurm09 <vidurm09@gmail.com>
2015-07-14 16:34:06 -0700

    Increased reusability of code

--- sampleCode/flyWheel.c
+++ sampleCode/flyWheel.c
@@ -12,21 +12,22 @@
 // global variables for tracking speed of flywheel 
 bool isRampedUp = false;
 int outakeSpeed = 0;
+int maxSpeed = 80; //The maximum speed of the flywheel (max value is 127)
+int minSpeed = 65;
 
-// sets the speed of the flywheel 
+// sets the speed of the flywheel **Change the motor names to your own!**
 void outake(int power)
 {
 	motor[leftBackOut] = power;
 	motor[leftFrontOut] = power;
 	motor[rightBackOut] = power;
 	motor[rightFrontOut]= power;
-
 }
 
-// raises the flywheel up to a speed of 80 in 80*time milliseconds 
+// raises the flywheel up to a speed of maxSpeed in maxSpeed*time milliseconds 
 void rampUp( int time )
 {
-	for(int i = 0; i <= 80; i++)
+	for(int i = outakeSpeed; i <= maxSpeed; i++)
 	{
 		outakeSpeed = i;
 		outake(outakeSpeed);
@@ -71,7 +72,7 @@ task usercontrol()
 		// raise speed in increments of 3 
 		if( vexRT[Btn7U] && time1[T1] > 250)
 		{
-			if( outakeSpeed < 80)
+			if( outakeSpeed < maxSpeed)
 			{
 				outakeSpeed = outakeSpeed + 3;
 				outake(outakeSpeed);
@@ -83,7 +84,7 @@ task usercontrol()
 		// slow speed in increments of 3 
 		else if (vexRT[Btn7D] && time1[T1] > 250)
 		{
-			if( outakeSpeed > 65)
+			if( outakeSpeed > minSpeed)
 			{
 				outakeSpeed = outakeSpeed - 3;
 				outake(outakeSpeed);
